9c2343ae33ad67277fbaadd71c6d5f4e19ca1477
Daniil VEDISHCHEV <daniil.vedishchev@gmail.com>
2026-05-07 14:28:45 +0200

    project init

---
+++ build/CMakeFiles/ORACULUM.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ORACULUM.dir/main/oraculum.cpp.o"
+  "CMakeFiles/ORACULUM.dir/main/oraculum.cpp.o.d"
+  "ORACULUM"
+  "ORACULUM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ORACULUM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
