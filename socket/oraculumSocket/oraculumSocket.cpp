6aa74a18a04a742fd7f32f65e8cb6f0978463dad
Daniil VEDISHCHEV <daniil.vedishchev@gmail.com>
2026-05-13 11:37:43 +0200

    upd: established socket

--- socket/oraculumSocket/oraculumSocket.cpp
+++ socket/oraculumSocket/oraculumSocket.cpp
@@ -1,4 +1,4 @@
-#include "socket/oraculumSocket/socket.hpp"
+#include "socket/oraculumSocket/oraculumSocket.hpp"
 
 OraculumSocket::OraculumSocket(const std::string& providerName, const std::string& connectorUrl) 
 : socket_{},connectorUrl_(connectorUrl) {
